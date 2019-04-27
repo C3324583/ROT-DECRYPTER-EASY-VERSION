6cb239ebedba5a229d6e9935f2f4b7e8c4d98d8d
William Millard Jorda <c3324583@uon.edu.au>
2019-04-27 08:25:49 +0000

    kc

--- rotdecryptereasy.c
+++ rotdecryptereasy.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+void rotation(int x, char text[]);
+void rotationdefault(char text[]);
+ 
 int main(){
     //initialisation of variables and arrays
 	char text[10000];
 	int number_of_letter[127] = {0};
-	int n, i, x, y, ascii, rotationkey, MAX = 0, common_letter;
+	int n, i, x, s=1, ascii, first=0, second=0, third=0, fourth=0, fifth=0;
 	
 	printf("Enter message: ");//prompts the user to imput a message
 	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
@@ -29,28 +31,142 @@ int main(){
         } 
 	}
 	
-	for (x=0; x<126; x++){
-	    if(number_of_letter[x] > MAX){
-	        MAX = number_of_letter[x];
-	        common_letter = x;
+	for (x=0; x<126; ++x){
+	    if(number_of_letter[x] > number_of_letter[first]){
+	        first = x;
 	    }
 	}
-
-	rotationkey = common_letter - 'E';
 	
-    for(y = 0; text[y] != '\0'; y++){
-        if(text[y] >= 'A' && text[y] <= 'Z'){
-    		text[y] = text[y] - rotationkey;
-    		if(text[y]>'Z'){
-		        text[y] = text[y] - 26;
+	for (x=0; x<126; ++x){
+	    if(x != first && number_of_letter[x] <= number_of_letter[first] && number_of_letter[x] > number_of_letter[second]){
+	        second = x;
+	    }
+	}
+	
+    for (x=0; x<126; ++x){
+	    if(x != first && x != second && number_of_letter[x] <= number_of_letter[second] && number_of_letter[x] > number_of_letter[third]){
+	        third = x;
+	    }
+	}	
+	
+	for (x=0; x<126; ++x){
+	    if(x != first && x != second && x != third && number_of_letter[x] <= number_of_letter[third] && number_of_letter[x] > number_of_letter[fourth]){
+	        fourth = x;
+	    }
+	}	
+	
+	for (x=0; x<126; ++x){
+	    if(x != first && x != second && x != third && x != fourth && number_of_letter[x] <= number_of_letter[fourth] && number_of_letter[x] > number_of_letter[fifth]){
+	        fifth = x;
+	    }
+	}
+	
+/*	int rotationkey = first - 'E';
+    for(int i=0; text[i] != '\0'; i++){
+        if(text[i] >= 'A' && text[i] <= 'Z'){
+    		text[i] = text[i] - rotationkey;
+    		if(text[i]>'Z'){
+		        text[i] = text[i] - 26;
 		    }
-		    if(text[y]<'A'){
-		        text[y] = text[y] + 26;
+		    if(text[i]<'A'){
+		        text[i] = text[i] + 26;
+		    }    
+		}
+    }
+    printf("Is this the correct message: %s\n", text); //prints the rotated message to the screen
+    rotationkey = second - 'E' - rotationkey;
+    for(int i=0; text[i] != '\0'; i++){
+        if(text[i] >= 'A' && text[i] <= 'Z'){
+    		text[i] = text[i] - rotationkey;
+    		if(text[i]>'Z'){
+		        text[i] = text[i] - 26;
+		    }
+		    if(text[i]<'A'){
+		        text[i] = text[i] + 26;
+		    }    
+		}
+    }
+    printf("Is this the correct message: %s\n", text); //prints the rotated message to the screen
+    rotationkey = third - 'E' - rotationkey;
+    for(int i=0; text[i] != '\0'; i++){
+        if(text[i] >= 'A' && text[i] <= 'Z'){
+    		text[i] = text[i] - rotationkey;
+    		if(text[i]>'Z'){
+		        text[i] = text[i] - 26;
+		    }
+		    if(text[i]<'A'){
+		        text[i] = text[i] + 26;
+		    }    
+		}
+    }
+    printf("Is this the correct message: %s\n", text); //prints the rotated message to the screen
+    
+    return 0;
+}*/
+
+	printf("%d %d %d %d %d \n",first,second,third,fourth,fifth);
+    
+    for(s=1; s<7; s++){
+    switch(s){
+        case 1: rotation(first, text);break;
+        case 2: rotation(second, text);break;
+        case 3: rotation(third, text);break;
+        case 4: rotation(fourth, text);break;
+        case 5: rotation(fifth, text);break;
+        default: printf("My code is shit but one of these should be correct:\n");rotationdefault(text);
+    }
+    }
+	return 0;
+}
+
+
+
+
+void rotation(int x, char text[]){
+    char txt[10000];
+    for(int y=0;text[y] != '\0'; y++){
+        txt[y]=text[y];
+    }
+    int rotationkey = x - 'E';
+	int success;
+    for(int i=0; txt[i] != '\0'; i++){
+        if(txt[i] >= 'A' && txt[i] <= 'Z'){
+    		txt[i] = txt[i] - rotationkey;
+    		if(txt[i]>'Z'){
+		        txt[i] = txt[i] - 26;
+		    }
+		    if(txt[i]<'A'){
+		        txt[i] = txt[i] + 26;
 		    }    
 		}
     }
 
-	printf("The decrypted message is: %s", text); //prints the rotated message to the screen	
-		
-	return 0;
+	printf("Is this the correct message: %s\n", txt); //prints the rotated message to the screen
+	printf("Yes - Press 1 and then <enter> \nNo - Press 0 and then <enter> \n");
+	scanf("%d", &success);
+	if(success == 1){
+	    exit(0);
+    }
 }
+
+
+
+//this function prints all possible rotation keys to the screen if the first function fails to find the correct rotation
+void rotationdefault(char text[]){
+    char txt[10000];
+    for(int y=0;text[y] != '\0'; y++){
+        txt[y]=text[y];
+    }
+
+    for(int x=0; x<=25; x++){
+        for(int i=0; txt[i] != '\0'; i++){
+            if(txt[i] >= 'A' && txt[i] <= 'Z'){
+        		txt[i] = txt[i] + 1;
+    	    	if(txt[i]>'Z'){
+    		        txt[i] = txt[i] - 26;
+    		    }
+    		}
+	    }
+	    printf("%s\n", txt);
+    }
+} 
